77f4228cda3f5b77f122a9f42130319d248e0268
andrewbituin <fandrewbituin@gmail.com>
2019-02-11 10:42:07 -0800

    map in BookList is working

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,23 +2,25 @@ import React from "react";
 
 export default class BookList extends React.Component {
   displayBookList = books => {
-    console.log(books);
-    const newBookList = books.map(book => {
-      return (
-        <li className={book.volumeInfo.title}>
-          <img
-            src={book.imageLinks.smallThumbnail}
-            alt="picture of book cover"
-          />
-          <h3>{book.volumeInfo.title}</h3>
-          <h4>{book.volumeInfo.authors}</h4>
-          <section>{book.saleInfo.retailPrice.amount}</section>
-          <p>{book.volumeInfo.description}</p>
-        </li>
-      );
-    });
-    console.log(newBookList);
-    return newBookList;
+    if (!books.items) {
+      return "";
+    } else {
+        const newBookList = books.items.map(book => {
+        return (
+          <li className={book.volumeInfo.title}>
+            <img
+              src={book.volumeInfo.imageLinks.smallThumbnail}
+              alt="picture of book cover"
+            />
+            <h3>{book.volumeInfo.title}</h3>
+            <h4>{book.volumeInfo.authors}</h4>
+            <section>price</section>
+            <p>{book.volumeInfo.description}</p>
+          </li>
+        );
+      });
+      return newBookList;
+    }
   };
 
   render() {
